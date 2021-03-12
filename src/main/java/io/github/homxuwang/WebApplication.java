33aa234442ea799888693872c2dd385adee65b94
homxu <wanghongxu1994@qq.com>
2021-03-12 16:17:35 +0800

    init: project

---
+++ src/main/java/io/github/homxuwang/WebApplication.java
@@ -0,0 +1,16 @@
+package io.github.homxuwang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author homxu
+ * @Date 2021/3/12 13:56
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
+    }
+}
---
+++ src/main/java/io/github/homxuwang/controller/TestController.java
@@ -0,0 +1,19 @@
+package io.github.homxuwang.controller;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author homxu
+ * @Date 2021/3/12 13:59
+ * @Version 1.0
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    private static final Logger LOGGER = LogManager.getLogger(TestController.class);
+
+}
